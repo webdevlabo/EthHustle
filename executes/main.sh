61fbaf99ae0928fb29262fdc1cb44889fb8601f4
webdevlabo <webdevlabo@gmail.com>
2018-01-13 08:53:09 +0200

    Update

--- executes/main.sh
+++ executes/main.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-value=$(</var/new/MoneroHustle/executes/mywallet.txt)
+value=$(</var/new/EthHustle/executes/mywallet.txt)
 
 export GPU_MAX_ALLOC_PERCENT=100
 /var/new/EthHustle/ethdcrminer64 -epool us1.ethermine.org:4444 -ewal "$value" -epsw x -mode 1 -allpools 1 -tt 68
