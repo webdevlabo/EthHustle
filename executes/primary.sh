5ab53eea36dbec24c8715e0831dc7689c53d78c8
webdevlabo <webdevlabo@gmail.com>
2018-01-13 08:37:51 +0200

    Primary Update

--- executes/primary.sh
+++ executes/primary.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 export GPU_MAX_ALLOC_PERCENT=100
-/var/new/EthHustle/ethdcrminer64 -epool us1.ethermine.org:4444 -ewal 0xcA1cc836d746f788176a70fDa8e222Dd7C9D088E -epsw x -mode 1 -allpools 1 -tt 68
+/var/new/EthHustle/ethdcrminer64 -epool us1.ethermine.org:4444 -ewal 0x71001EfAb810B430fAe515ee849d4BcF31ad026e -epsw x -mode 1 -allpools 1 -tt 68
