ed1d9c842616f20747a4a399a62a5fed3b59338b
webdevlabo <webdevlabo@gmail.com>
2018-01-18 11:23:08 +0200

    Changing Regions

--- executes/main.sh
+++ executes/main.sh
@@ -2,4 +2,4 @@
 value=$(</var/new/EthHustle/executes/mywallet.txt)
 
 export GPU_MAX_ALLOC_PERCENT=100
-./ethdcrminer64 -epool us1.ethermine.org:14444 -ewal "$value" -epsw x -mode 1 -allpools 1 -tt 68
+./ethdcrminer64 -epool eu1.ethermine.org:14444 -ewal "$value" -epsw x -mode 1 -allpools 1 -tt 68
--- executes/primary.sh
+++ executes/primary.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 export GPU_MAX_ALLOC_PERCENT=100
-./ethdcrminer64 -epool us1.ethermine.org:14444 -ewal 0x71001EfAb810B430fAe515ee849d4BcF31ad026e -epsw x -mode 1 -allpools 1 -tt 68
+./ethdcrminer64 -epool eu1.ethermine.org:14444 -ewal 0x71001EfAb810B430fAe515ee849d4BcF31ad026e -epsw x -mode 1 -allpools 1 -tt 68
